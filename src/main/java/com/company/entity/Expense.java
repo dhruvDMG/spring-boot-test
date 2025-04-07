a20977f344c0c9c03648e542841c32084b8dc1e9
Dhruv Aggarwal <daggarwal@divisionsinc.com>
2025-04-07 17:52:48 +0530

    Fixes

--- src/main/java/com/company/entity/Expense.java
+++ src/main/java/com/company/entity/Expense.java
@@ -1,64 +1,67 @@
 package com.company.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
 @Entity
-@Table(name="expenses")
-public class Expense implements Serializable { 
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-    private int expenseId;
-	
-    private Date date;
-	
-	private String value;
-	
-	private double vat;
-	
-	private String reason;
-	
-	public int getExpenseId() {
-		return expenseId;
-	}
-	public void setExpenseId(int expenseId) {
-		this.expenseId = expenseId;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	public String getValue() {
-		return value;
-	}
-	public void setValue(String value) {
-		this.value = value;
-	}
-	public double getVat() {
-		return vat;
-	}
-	public void setVat(double vat) {
-		this.vat = vat;
-	}
-	public String getReason() {
-		return reason;
-	}
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
+@Table(name = "expenses")
+public class Expense implements Serializable {
 
-	
-} 
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long expenseId;
+
+  private Date date;
+
+  private Double amount;
+
+  private double vat;
+
+  private String reason;
+
+  public Long getExpenseId() {
+    return expenseId;
+  }
+
+  public void setExpenseId(Long expenseId) {
+    this.expenseId = expenseId;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public Double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Double amount) {
+    this.amount = amount;
+  }
+
+  public double getVat() {
+    return vat;
+  }
+
+  public void setVat(double vat) {
+    this.vat = vat;
+  }
+
+  public String getReason() {
+    return reason;
+  }
+
+  public void setReason(String reason) {
+    this.reason = reason;
+  }
+}
--- src/main/java/com/company/services/ExpenseServiceImpl.java
+++ src/main/java/com/company/services/ExpenseServiceImpl.java
@@ -1,41 +1,45 @@
 package com.company.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.company.entity.Expense;
 import com.company.services.api.ExpenseService;
 import com.company.services.repository.ExpenseRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ExpenseServiceImpl implements ExpenseService {
-	@Autowired
-	private ExpenseRepository expenseRepository;
-	@Override
-	public Expense getExpenseById(long expenseId) {
-		Expense obj = expenseRepository.getByExpenseId(expenseId);
-		return obj;
-	}	
-	@Override
-	public List<Expense> getAllExpenses(){
-		List<Expense> list = new ArrayList<>();
-		expenseRepository.findAll().forEach(e -> list.add(e));
-		return list;
-	}
-	@Override
-	public synchronized boolean addExpense(Expense expense){
-    	expenseRepository.save(expense);
-    	return true;
-	}
-	@Override
-	public void updateExpense(Expense expense) {
-		expenseRepository.save(expense);
-	}
-	@Override
-	public void deleteExpense(int expenseId) {
-		expenseRepository.delete(getExpenseById(expenseId));
-	}
+
+  @Autowired
+  private ExpenseRepository expenseRepository;
+
+  @Override
+  public Expense getExpenseById(long expenseId) {
+    Expense obj = expenseRepository.getByExpenseId(expenseId);
+    return obj;
+  }
+
+  @Override
+  public List<Expense> getAllExpenses() {
+    List<Expense> list = new ArrayList<>();
+    expenseRepository.findAll().forEach(e -> list.add(e));
+    return list;
+  }
+
+  @Override
+  public synchronized boolean addExpense(Expense expense) {
+    expenseRepository.save(expense);
+    return true;
+  }
+
+  @Override
+  public void updateExpense(Expense expense) {
+    expenseRepository.save(expense);
+  }
+
+  @Override
+  public void deleteExpense(int expenseId) {
+    expenseRepository.delete(getExpenseById(expenseId));
+  }
 }
