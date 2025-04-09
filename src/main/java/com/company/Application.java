e3414aa369cdbb69118e9a4ec662fda5e7ef835c
Dhruv Aggarwal <daggarwal@divisionsinc.com>
2025-04-09 09:51:19 +0530

    Fixes

--- src/main/java/com/company/Application.java
+++ src/main/java/com/company/Application.java
@@ -21,27 +21,27 @@ public class Application {
     SpringApplication.run(Application.class, args);
   }
 
-	@Bean
-	CommandLineRunner init(UserRepository userRepository) {
-		return args -> {
-			Faker faker = new Faker();
-			List<User> users = new ArrayList<>();
-
-			for (int i = 0; i < 100_000; i++) {
-				String first = faker.name().firstName();
-				String middle = faker.name().firstName();
-				String last = faker.name().lastName();
-				String username = faker.name().username();
-				String email = username + "@example.com";
-				String password = UUID.randomUUID().toString().substring(0, 8);
-				LocalDate dob = faker.date().birthday(18, 60).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-				LocalDateTime createdAt = LocalDateTime.now().minusDays(faker.number().numberBetween(0, 365));
-				boolean isActive = faker.bool().bool();
-
-				users.add(new User(first, middle, last, username, email, password, dob, createdAt, isActive));
-			}
-
-			userRepository.saveAll(users);
-		};
-	}
+//	@Bean
+//	CommandLineRunner init(UserRepository userRepository) {
+//		return args -> {
+//			Faker faker = new Faker();
+//			List<User> users = new ArrayList<>();
+//
+//			for (int i = 0; i < 100_000; i++) {
+//				String first = faker.name().firstName();
+//				String middle = faker.name().firstName();
+//				String last = faker.name().lastName();
+//				String username = faker.name().username();
+//				String email = username + "@example.com";
+//				String password = UUID.randomUUID().toString().substring(0, 8);
+//				LocalDate dob = faker.date().birthday(18, 60).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+//				LocalDateTime createdAt = LocalDateTime.now().minusDays(faker.number().numberBetween(0, 365));
+//				boolean isActive = faker.bool().bool();
+//
+//				users.add(new User(first, middle, last, username, email, password, dob, createdAt, isActive));
+//			}
+//
+//			userRepository.saveAll(users);
+//		};
+//	}
 }            
